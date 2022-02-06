3b7c93854741994a9894333c61df7f15f2ba1810
divadluca14 <divadluca14@gmail.com>
2022-02-06 19:57:19 +0200

    Restrict one shift per day

--- time_logger/models.py
+++ time_logger/models.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+from marshmallow import fields
+from marshmallow.validate import Range
+
 from time_logger import db, ma
 
 
@@ -12,33 +15,48 @@ class Worker(db.Model):
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 
-class WorkerSchema(ma.SQLAlchemySchema):
+class WorkerSchema(ma.Schema):
     class Meta:
         model = Worker
 
-    id = ma.auto_field()
-    lname = ma.auto_field()
-    fname = ma.auto_field()
-    timestamp = ma.auto_field()
+    id = fields.Integer(required=True)
+    lname = fields.Str(required=True)
+    fname = fields.Str(required=True)
+    timestamp = fields.DateTime(allow_none=True)
 
+
+shifts_per_day = {
+    '1': {
+        'start_hour': 8,
+        'end_hour': 16
+    },
+    '2': {
+        'start_hour': 16,
+        'end_hour': 0
+    },
+    '3': {
+        'start_hour': 0,
+        'end_hour': 8
+    }
+}
 
 class Shift(db.Model):
     __tablename__ = 'shift'
 
     id = db.Column(db.Integer, primary_key=True)
-    start_date = db.Column(db.DateTime, default=datetime.utcnow)
-    end_date = db.Column(db.DateTime)
+    shift_type = db.Column(db.Integer)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
 
     worker_id = db.Column(db.Integer, db.ForeignKey('worker.id'), nullable=False)
     worker = db.relationship("Worker", backref=db.backref('shifts', lazy=True))
 
 
-class ShiftSchema(ma.SQLAlchemyAutoSchema):
+class ShiftSchema(ma.Schema):
     class Meta:
         model = Shift
         include_fk = True
 
-    id = ma.auto_field()
-    start_date = ma.auto_field()
-    end_date = ma.auto_field()
-    worker_id = ma.auto_field()
+    id = fields.Integer(required=True)
+    shift_type = fields.Integer(validate=Range(min=1, max=4))
+    created = fields.DateTime(allow_none=False)
+    worker_id = fields.Integer(required=True)
--- time_logger/views.py
+++ time_logger/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import abort, jsonify, request, render_template
 from marshmallow import ValidationError
 
@@ -36,6 +38,19 @@ def get_shift(worker_id, shift_id):
         abort(404, 'Shift not found for Id: {shift_id}'.format(shift_id=shift_id))
 
 
+def shift_dosent_exist_for_that_day(worker_id, data):
+    result = False
+    worker = Worker.query.get(worker_id)
+    existing_shift = Shift.query.with_parent(worker) \
+        .filter(Shift.created.strftime('%Y-%m-%d') == data['created'][:12]) \
+        .one_or_none()
+
+    if existing_shift:
+        result = True
+
+    return result
+
+
 @app.post("/workers/<int:worker_id>/shifts")
 def add_shift(worker_id):
     if request.is_json:
@@ -47,23 +62,15 @@ def add_shift(worker_id):
         except ValidationError as err:
             return err.messages, 422
 
-        worker = Worker.query.get(worker_id)
-        existing_shift = Shift.query.with_parent(worker) \
-            .filter(Shift.start_date == data['start_date']) \
-            .filter(Shift.end_date == data['end_date']) \
-            .one_or_none()
-
-        if existing_shift is None:
-            new_shift = Shift(start_date=data['start_date'], end_date=data['end_date'], worker_id=worker_id)
+        if shift_dosent_exist_for_that_day:
+            new_shift = Shift(created=data['created'], worker_id=worker_id)
             db.session.add(new_shift)
             db.session.commit()
             return data, 201
         else:
             abort(
                 409,
-                "Shift {start_date} {end_date} exists already".format(
-                    start_date=json['start_date'], end_date=json['end_date']
-                ),
+                "Shift for day {date} exists already".format(date=data['created']),
             )
 
     return {"error": "Request must be JSON"}, 415
